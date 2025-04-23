2942ca0d07299c72858218a6fecb34341c12aa10
meher.janapareddy <yamini.kamasani@madd.org>
2025-04-22 21:20:06 -0400

    removed unused code from build.sh

--- ytsummary-api/build.sh
+++ ytsummary-api/build.sh
@@ -2,10 +2,3 @@
 apt-get update && apt-get install -y ffmpeg
 pip install -r ytsummary-api/requirements.txt
 python3 -m pip install yt-dlp
-
-if [ -n "$COOKIE_B64" ]; then
-  echo "$COOKIE_B64" | base64 -d > /app/cookies.txt
-  echo "✅ cookies.txt created from COOKIE_B64 env variable"
-else
-  echo "⚠️ COOKIE_B64 not set. YouTube downloads may fail."
-fi
